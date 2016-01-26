f9ec9f79925640f5770f5567d19acbdb602bdae1
eydlin <a.aidlin@gmail.com>
2016-01-26 12:03:49 +0300

    refactor(JepRiaShowcase): Из web.xml убрана закомментированная секция кода, ответственная за интеграцию JavaSSO и CAS.
    feat(JepRiaShowcase): При установке на Weblogic вместо weblogic.jar используется wlfullclient.jar.
    
    chore(JepRiaShowcase): Актуализирована конфигурация сборки debug.
    
    chore(JepRiaShowcase): build.xml актуализирован с учётом изменения структуры директорий cas-client.
    
    chore(JepRiaShowcase): Актуализированы версии JepRia, JepRiaReport, JepRiaToolkit, cas-client в build.properties.

--- Module/JepRiaShowcase/App/src/java/com/technology/jep/jepriashowcase/goods/shared/text/GoodsText.java
+++ Module/JepRiaShowcase/App/src/java/com/technology/jep/jepriashowcase/goods/shared/text/GoodsText.java
@@ -2,7 +2,7 @@ package com.technology.jep.jepriashowcase.goods.shared.text;
 
 /**
  * Interface to represent the constants contained in resource bundle:
- * 	'D:/git/Jep/Module/JepRiaShowcase/App/src/java/com/technology/jep/jepriashowcase/goods/shared/text/GoodsText.properties'.
+ * 	'E:/git/javaenterpriseplatform/Module/JepRiaShowcase/App/src/java/com/technology/jep/jepriashowcase/goods/shared/text/GoodsText.properties'.
  */
 public interface GoodsText extends com.google.gwt.i18n.client.Constants {
   
