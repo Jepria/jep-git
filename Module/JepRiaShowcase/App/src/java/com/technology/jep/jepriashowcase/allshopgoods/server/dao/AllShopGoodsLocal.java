552e72a8dbcee81ad8d656128f2651440cd08e77
eydlin <a.aidlin@gmail.com>
2016-03-17 13:16:15 +0300

    feat(JepRiaShowcase): Из модуля AllShopGoods удалены интерфейсы EJB.

--- Module/JepRiaShowcase/App/src/java/com/technology/jep/jepriashowcase/allshopgoods/server/dao/AllShopGoodsLocal.java
+++
@@ -1,9 +0,0 @@
-package com.technology.jep.jepriashowcase.allshopgoods.server.dao;
- 
-import javax.ejb.Local;
-
-import com.technology.jep.jepriashowcase.allshopgoods.server.dao.AllShopGoods;
- 
-@Local
-public interface AllShopGoodsLocal extends AllShopGoods {
-}
--- Module/JepRiaShowcase/App/src/java/com/technology/jep/jepriashowcase/allshopgoods/server/dao/AllShopGoodsRemote.java
+++
@@ -1,9 +0,0 @@
-package com.technology.jep.jepriashowcase.allshopgoods.server.dao;
- 
-import javax.ejb.Remote;
-
-import com.technology.jep.jepriashowcase.allshopgoods.server.dao.AllShopGoods;
- 
-@Remote
-public interface AllShopGoodsRemote extends AllShopGoods {
-}
