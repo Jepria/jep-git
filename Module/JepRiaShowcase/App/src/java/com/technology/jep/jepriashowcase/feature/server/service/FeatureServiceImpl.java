19ac0633ebf3555917a3d44650a5d933592b4222
eydlin <a.aidlin@gmail.com>
2016-07-14 12:01:51 +0300

    refactor(JepRiaShowcase): Добавлены серверные фабрики модулей.

--- Module/JepRiaShowcase/App/src/java/com/technology/jep/jepriashowcase/feature/server/service/FeatureServiceImpl.java
+++ Module/JepRiaShowcase/App/src/java/com/technology/jep/jepriashowcase/feature/server/service/FeatureServiceImpl.java
@@ -1,12 +1,9 @@
 package com.technology.jep.jepriashowcase.feature.server.service;
- 
-import static com.technology.jep.jepriashowcase.feature.server.FeatureServerConstant.DATA_SOURCE_JNDI_NAME;
 
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
-import com.technology.jep.jepria.server.ServerFactory;
 import com.technology.jep.jepria.server.service.JepDataServiceServlet;
+import com.technology.jep.jepriashowcase.feature.server.FeatureServerFactory;
 import com.technology.jep.jepriashowcase.feature.server.dao.Feature;
-import com.technology.jep.jepriashowcase.feature.server.dao.FeatureDao;
 import com.technology.jep.jepriashowcase.feature.shared.record.FeatureRecordDefinition;
 import com.technology.jep.jepriashowcase.feature.shared.service.FeatureService;
  
@@ -16,6 +13,6 @@ public class FeatureServiceImpl extends JepDataServiceServlet<Feature> implement
   private static final long serialVersionUID = 1L;
  
   public FeatureServiceImpl() {
-    super(FeatureRecordDefinition.instance, new ServerFactory<Feature>(new FeatureDao(), DATA_SOURCE_JNDI_NAME));
+    super(FeatureRecordDefinition.instance, FeatureServerFactory.instance);
   }
 }
--- Module/JepRiaShowcase/App/src/java/com/technology/jep/jepriashowcase/requestprocess/server/service/RequestProcessServiceImpl.java
+++ Module/JepRiaShowcase/App/src/java/com/technology/jep/jepriashowcase/requestprocess/server/service/RequestProcessServiceImpl.java
@@ -1,12 +1,9 @@
 package com.technology.jep.jepriashowcase.requestprocess.server.service;
  
-import static com.technology.jep.jepriashowcase.requestprocess.server.RequestProcessServerConstant.DATA_SOURCE_JNDI_NAME;
-
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
-import com.technology.jep.jepria.server.ServerFactory;
 import com.technology.jep.jepria.server.service.JepDataServiceServlet;
+import com.technology.jep.jepriashowcase.requestprocess.server.RequestProcessServerFactory;
 import com.technology.jep.jepriashowcase.requestprocess.server.dao.RequestProcess;
-import com.technology.jep.jepriashowcase.requestprocess.server.dao.RequestProcessDao;
 import com.technology.jep.jepriashowcase.requestprocess.shared.record.RequestProcessRecordDefinition;
 import com.technology.jep.jepriashowcase.requestprocess.shared.service.RequestProcessService;
  
@@ -16,6 +13,6 @@ public class RequestProcessServiceImpl extends JepDataServiceServlet<RequestProc
   private static final long serialVersionUID = 1L;
  
   public RequestProcessServiceImpl() {
-    super(RequestProcessRecordDefinition.instance, new ServerFactory<RequestProcess>(new RequestProcessDao(), DATA_SOURCE_JNDI_NAME));
+    super(RequestProcessRecordDefinition.instance, RequestProcessServerFactory.instance);
   }
 }
