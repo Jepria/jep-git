270a3f58c9fccab6d3ab7ad517f5a134bdedbc28
rozakharov <rozakharov@gmail.com>
2017-04-13 13:37:31 +0300

    chore(JepRiaReport): сборка на jdk1.8.0_77, JepRia 10, актуализация файлов *.properties fix(JepRiaReport): В PdfServlet.java метод service заменен на doGet, иначе ошибка 405 (HTTP method GET is not supported by this URL) в Tomcat.

--- Module/JepRiaReport/App/src/java/com/technology/jep/jepriareport/server/JepReportDataSource.java
+++ Module/JepRiaReport/App/src/java/com/technology/jep/jepriareport/server/JepReportDataSource.java
@@ -16,96 +16,96 @@ import com.technology.jep.jepria.shared.record.JepRecord;
  * Источник данных для работы с SimpleViewObject
  */
 public class JepReportDataSource implements JRDataSource {
-	
-	/**
-	 * Штатный режим
-	 */
-	private final static int DEFAULT_MODE = 0;
+  
+  /**
+   * Штатный режим
+   */
+  private final static int DEFAULT_MODE = 0;
 
-	protected JepReportRecords recordList = null;
+  protected JepReportRecords recordList = null;
 
-	/**
-	 * Режим тестирования исчерпания памяти
-	 */
-	private final static int OUT_OF_MEMORY_TEST_MODE = 1;
-	private final static int mode = DEFAULT_MODE;
-	private final static int MAX_ROW_NUMBER_TEST_MODE = 100000;
-	private int currentRowNumber = 0;
+  /**
+   * Режим тестирования исчерпания памяти
+   */
+  private final static int OUT_OF_MEMORY_TEST_MODE = 1;
+  private final static int mode = DEFAULT_MODE;
+  private final static int MAX_ROW_NUMBER_TEST_MODE = 100000;
+  private int currentRowNumber = 0;
 
-	public JepReportDataSource(JepReportRecords records) {
-		this.recordList = records;
-		this.recordList.beforeFirst();
-	}
-	
-	/**
-	 * Конструктор источника данных используется для анализа дизайна отчёта с
-	 * целью динамической настройки. При этом необходимо, чтобы дизайн отчёта
-	 * (.jrxml) размещался в дистрибутиве модуля (ear) в директории файлов
-	 * отчётов.
-	 * 
-	 * @param records  		   представление списочных данных
-	 * @param reportDesignPath путь файла jrxml, содержащего дизайн отчёта
-	 * @throws JRException
-	 */
-	public JepReportDataSource(JepReportRecords records, String reportDesignPath)
-			throws JRException {
-		this(records);
-		// Настройка view на параметры подотчёта
-		JasperDesign jasperDesign = JRXmlLoader.load(reportDesignPath);
-		int columnNumber = jasperDesign.getColumnCount();
-		if (columnNumber > 1) {
-			// Оба способа размещения "сначала по-вертикали" и "сначала по-горизонтали" в
-			// JasperReports выполняются в режиме JRReport.PRINT_ORDER_HORIZONTAL (workaround).
-			// При способе "сначала по-вертикали" горизонтальный режим
-			// JasperReports компенсируется горизонтальным режимом SimpleViewObject
-			if (jasperDesign.getPrintOrderValue() == PrintOrderEnum.VERTICAL) {
-				records.setPrintOrder(JepReportRecords.PrintOrder.HORIZONTAL);
-				jasperDesign.setPrintOrder(PrintOrderEnum.VERTICAL);
-				JasperCompileManager.compileReportToFile(jasperDesign, 
-						reportDesignPath.replaceAll("jrxml", "jasper"));
-			} else {
-				records.setPrintOrder(JepReportRecords.PrintOrder.VERTICAL);
-			}
-			records.setColumnNumber(columnNumber);
-		}
-	}
+  public JepReportDataSource(JepReportRecords records) {
+    this.recordList = records;
+    this.recordList.beforeFirst();
+  }
+  
+  /**
+   * Конструктор источника данных используется для анализа дизайна отчёта с
+   * целью динамической настройки. При этом необходимо, чтобы дизайн отчёта
+   * (.jrxml) размещался в дистрибутиве модуля (ear) в директории файлов
+   * отчётов.
+   * 
+   * @param records         представление списочных данных
+   * @param reportDesignPath путь файла jrxml, содержащего дизайн отчёта
+   * @throws JRException
+   */
+  public JepReportDataSource(JepReportRecords records, String reportDesignPath)
+      throws JRException {
+    this(records);
+    // Настройка view на параметры подотчёта
+    JasperDesign jasperDesign = JRXmlLoader.load(reportDesignPath);
+    int columnNumber = jasperDesign.getColumnCount();
+    if (columnNumber > 1) {
+      // Оба способа размещения "сначала по-вертикали" и "сначала по-горизонтали" в
+      // JasperReports выполняются в режиме JRReport.PRINT_ORDER_HORIZONTAL (workaround).
+      // При способе "сначала по-вертикали" горизонтальный режим
+      // JasperReports компенсируется горизонтальным режимом SimpleViewObject
+      if (jasperDesign.getPrintOrderValue() == PrintOrderEnum.VERTICAL) {
+        records.setPrintOrder(JepReportRecords.PrintOrder.HORIZONTAL);
+        jasperDesign.setPrintOrder(PrintOrderEnum.VERTICAL);
+        JasperCompileManager.compileReportToFile(jasperDesign, 
+            reportDesignPath.replaceAll("jrxml", "jasper"));
+      } else {
+        records.setPrintOrder(JepReportRecords.PrintOrder.VERTICAL);
+      }
+      records.setColumnNumber(columnNumber);
+    }
+  }
 
-	public JepReportDataSource(List<JepRecord> recordList,
-			String reportDesignPath) throws JRException {
-		this(new JepReportRecords(recordList), reportDesignPath);
-	}
+  public JepReportDataSource(List<JepRecord> recordList,
+      String reportDesignPath) throws JRException {
+    this(new JepReportRecords(recordList), reportDesignPath);
+  }
 
-	public boolean next() throws JRException {
-		boolean result = false;
-		switch (mode) {
-			case OUT_OF_MEMORY_TEST_MODE:
-				if (currentRowNumber++ < MAX_ROW_NUMBER_TEST_MODE) {
-					recordList.first(); // Возвращается один и тот же первый объект
-					result = true;
-					if (currentRowNumber % 1000 == 0) {
-						System.out.println("Получено " + currentRowNumber
-								+ " записей");
-					}
-				}
-				break;
-			default:
-				if (recordList.hasNext()) {
-					recordList.next();
-					result = true;
-				}
-				break;
-		}
-		return result;
-	}
+  public boolean next() throws JRException {
+    boolean result = false;
+    switch (mode) {
+      case OUT_OF_MEMORY_TEST_MODE:
+        if (currentRowNumber++ < MAX_ROW_NUMBER_TEST_MODE) {
+          recordList.first(); // Возвращается один и тот же первый объект
+          result = true;
+          if (currentRowNumber % 1000 == 0) {
+            System.out.println("Получено " + currentRowNumber
+                + " записей");
+          }
+        }
+        break;
+      default:
+        if (recordList.hasNext()) {
+          recordList.next();
+          result = true;
+        }
+        break;
+    }
+    return result;
+  }
 
-	public Object getFieldValue(JRField field) throws JRException {
-		String fieldName = field.getName();
-		return recordList.getCurrentValue(fieldName);
-	}
+  public Object getFieldValue(JRField field) throws JRException {
+    String fieldName = field.getName();
+    return recordList.getCurrentValue(fieldName);
+  }
 
-	public boolean beforeFirst() throws JRException {
-		this.recordList.beforeFirst();
-		return true;
-	}
+  public boolean beforeFirst() throws JRException {
+    this.recordList.beforeFirst();
+    return true;
+  }
 
 }
