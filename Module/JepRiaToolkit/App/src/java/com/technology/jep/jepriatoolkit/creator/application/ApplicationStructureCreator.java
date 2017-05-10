28ae8609b2f929db82ef5816ec680a40b23646f8
remitot <call9096463086@gmail.com>
2017-05-10 13:46:38 +0300

    revert(JepRiaToolkit): возвращена генерация tomcat/context.xml

--- Module/JepRiaToolkit/App/src/java/com/technology/jep/jepriatoolkit/creator/application/ApplicationStructureCreator.java
+++ Module/JepRiaToolkit/App/src/java/com/technology/jep/jepriatoolkit/creator/application/ApplicationStructureCreator.java
@@ -16,6 +16,7 @@ import static com.technology.jep.jepriatoolkit.util.JepRiaToolkitUtil.readFromJa
 import static com.technology.jep.jepriatoolkit.util.JepRiaToolkitUtil.replacePackageModuleNames;
 import static com.technology.jep.jepriatoolkit.util.JepRiaToolkitUtil.writeToFile;
 import static java.text.MessageFormat.format;
+import static com.technology.jep.jepria.server.JepRiaServerConstant.DEFAULT_DATA_SOURCE_JNDI_NAME;
 
 import java.io.File;
 import java.io.IOException;
@@ -63,6 +64,8 @@ public class ApplicationStructureCreator extends Task {
       createApplicationFileStructure();
       echoMessage("Generate web.xml");
       generateWebXML();
+      echoMessage("Generate tomcat/context.xml");
+      generateTomcatContextXml();
       echoMessage("Create Welcome page!");
       createWelcomePage();
       echoMessage("Generate xml for GWT-application");
@@ -276,7 +279,30 @@ public class ApplicationStructureCreator extends Task {
       )
     );
   }
-
+  
+  /**
+   * Создание tomcat/context.xml. <br/>
+   * TODO: Параметризовать appName в Realm в *Definition.xml, так как сейчас жестко зашит RFInfoDS.
+   */
+  private void generateTomcatContextXml() {
+    
+    Map<String, Object> realm = new HashMap<String, Object>();
+    realm.put(REALM_APPNAME_TEMPLATE_PARAMETER, 
+        DEFAULT_DATA_SOURCE_JNDI_NAME.substring(DEFAULT_DATA_SOURCE_JNDI_NAME.indexOf("/") + 1)); //bad view TODO;
+    makeDir(
+        format(getDefinitionProperty(TOMCAT_RESOURCE_DIRECTORY_TEMPLATE_PROPERTY, 
+            multipleConcat(PREFIX_DESTINATION_RESOURCE, "{0}/{1}/tomcat")),
+        application.getProjectPackage().toLowerCase(), application.getName().toLowerCase()));
+    
+    convertTemplateToFile(
+        getDefinitionProperty(TOMCAT_CONTEXT_XML_TEMPLATE_PROPERTY, "tomcatContext.ftl"), 
+        realm,
+        format(
+            getDefinitionProperty(TOMCAT_CONTEXT_XML_PATH_TEMPLATE_PROPERTY, 
+                multipleConcat(PREFIX_DESTINATION_RESOURCE, "{0}/{1}/tomcat/context.xml")), 
+            application.getProjectPackage().toLowerCase(), application.getName().toLowerCase()));
+  }
+  
   /**
    * Создание основного GWT.xml для всего приложения, описывающего его
    * структуру
