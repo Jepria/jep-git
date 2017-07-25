5730e636f0ee76773d19ec34868bda362b61fda3
almirus <almir.abrarov@gmail.com>
2017-07-25 18:30:45 +0300

    [*] пересобрано на JDK7 и с поддержкой jasperreports-6.3.0 [!] теперь имя выходного файла соответствует имени входного (ранее имя бралось из property отчета)

--- Module/JrCompiler/App/src/java/com/technology/jep/jrcompiler/JRCompiler.java
+++ Module/JrCompiler/App/src/java/com/technology/jep/jrcompiler/JRCompiler.java
@@ -12,13 +12,13 @@ import net.sf.jasperreports.engine.JasperCompileManager;
  * Предназначен для преобразования исходного файла определения отчёта в формате XML (как правило, имеющего расширение <code>*.jrxml</code>)
  * в результирующий файл, содержащий сериализованный вид объектного представления. Результирующий файл имеет расширение <code>*.jasper</code>,
  * размещается в той же директории, что исходный файл, и имеет такое же имя.
- * 
+ * <p>
  * <p>
  * <u>Правила вызова:</u><br>
  * <p>
  * <I><code>java -cp &lt;ScriptletAndDataSourceClasses&gt;;jrcompiler.jar com.technology.jep.jrcompiler.JRCompiler Report.jrxml</code></I>
  * <I><code>java -cp &lt;ScriptletAndDataSourceClasses&gt;;jrcompiler.jar com.technology.jep.jrcompiler.JRCompiler report_directory</code></I>
- * 
+ * <p>
  * <p>
  * <u>Примеры использования:</u>
  * <p>
@@ -33,63 +33,64 @@ import net.sf.jasperreports.engine.JasperCompileManager;
  * %JAVA_HOME%\bin\java -cp %CLASSES%;%JEP_REPORT_COMPILER%\lib\jrcompiler.jar com.technology.jep.jrcompiler.JRCompiler %RESOURCE_HOME%\%MODULE_PACKAGE%\report<br>
  * ...<br>
  * </code></I>
- * 
  */
 public class JRCompiler {
 
-  /**
-   * @param args параметры запуска
-   */
-  public static void main(String[] args) {
-    if(args.length < 1) {
-      showUsageError("Wrong parameter number.");
-    }
-        
-    try {
-      compile(new File(args[0]));
-    } catch (JRException e) {
-      showError(e.getMessage());
-    }
-  }
+	/**
+	 * @param args параметры запуска
+	 */
+	public static void main(String[] args) {
+		if (args.length < 1) {
+			showUsageError("Wrong parameter number.");
+		}
 
-  private static void compile(File srcFile) throws JRException {
-    if (srcFile.isFile()) {
-      // Передали 1 файл
-      String srcFileName = srcFile.getAbsolutePath();
-      try {
-        JasperCompileManager.compileReportToFile(srcFileName);
-      } catch (JRException e) {
-        showError(e.getMessage());
-      }
-    } else { // передали директорию
-      // Создание списка файлов, состоящих из исходников текущей директории и вложенных директорий
-      File[] files = srcFile.listFiles(new FilenameFilter() {
-        public boolean accept(File dir, String name) {
-          String path = dir + "/" + name;
-          return new File(path).isDirectory() || name.endsWith(".jrxml") ;
-        }
-      });
-      
-      if(files != null) {
-        for (File file : files) {
-          compile(file);
-        }
-      }
-    }
-  }
+		try {
+			compile(new File(args[0]));
+		} catch (JRException e) {
+			showError(e.getMessage());
+		}
+	}
 
-  private static void showError(String message) {
-    System.out.println("ERROR: " + message);
-  }
+	private static void compile(File srcFile) throws JRException {
+		if (srcFile.isFile()) {
+			// Передали 1 файл
+			String srcFileName = srcFile.getAbsolutePath();
+			// название берем не из отчета, а такое же как исходное имя шаблона
+			String destFileName = srcFileName.substring(0, srcFileName.lastIndexOf(".") + 1) + "jasper";
+			try {
+				JasperCompileManager.compileReportToFile(srcFileName, destFileName);
+			} catch (JRException e) {
+				showError(e.getMessage());
+			}
+		} else { // передали директорию
+			// Создание списка файлов, состоящих из исходников текущей директории и вложенных директорий
+			File[] files = srcFile.listFiles(new FilenameFilter() {
+				public boolean accept(File dir, String name) {
+					String path = dir + "/" + name;
+					return new File(path).isDirectory() || name.endsWith(".jrxml");
+				}
+			});
 
-  private static void showUsageError(String message) {
-    System.out.println(message);
-    System.out.println("Usage:");
-    System.out.println("java -jar jrcompiler <sourceFilePath>");
-    System.out.println("where:");
-    System.out.println("sourceFilePath:\t\tFull path of the file containing a report definition");
+			if (files != null) {
+				for (File file : files) {
+					compile(file);
+				}
+			}
+		}
+	}
 
-    System.exit(-1);
-  }
-  
+	private static void showError(String message) {
+		System.out.println("ERROR: " + message);
+	}
+
+	private static void showUsageError(String message) {
+		System.out.println(message);
+		System.out.println("Usage:");
+		System.out.println("java -jar jrcompiler <sourceFilePath>");
+		System.out.println("where:");
+		System.out.println("sourceFilePath:\t\tFull path of the file containing a report definition");
+
+		System.exit(-1);
+	}
+
 }
