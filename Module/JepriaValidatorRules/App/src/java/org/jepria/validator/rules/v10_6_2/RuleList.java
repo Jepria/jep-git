e5f62baa8e19af162c18b1305e290c243be3e019
remitot <call9096463086@gmail.com>
2018-02-16 15:13:32 +0300

    feat(JepriaValidatorRules): правила для версий 10.6.2; 10.7.0

---
+++ Module/JepriaValidatorRules/App/src/java/org/jepria/validator/rules/v10_6_2/RuleList.java
@@ -0,0 +1,9 @@
+package org.jepria.validator.rules.v10_6_2;
+
+public class RuleList extends org.jepria.validator.rules.v10_6_1.RuleList {
+
+  private static final long serialVersionUID = 1L;
+
+  public RuleList() {
+  }
+}
---
+++ Module/JepriaValidatorRules/App/src/java/org/jepria/validator/rules/v10_7_0/RuleList.java
@@ -0,0 +1,9 @@
+package org.jepria.validator.rules.v10_7_0;
+
+public class RuleList extends org.jepria.validator.rules.v10_6_2.RuleList {
+
+  private static final long serialVersionUID = 1L;
+
+  public RuleList() {
+  }
+}
