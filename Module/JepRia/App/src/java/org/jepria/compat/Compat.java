d61b58b4d89476735d5e15940e2a9f4445dcb921
remitot <call9096463086@gmail.com>
2018-10-22 19:00:11 +0300

    chore(Jepria): Compat: convertRecordDefinition

--- Module/JepRia/App/src/java/org/jepria/compat/Compat.java
+++ Module/JepRia/App/src/java/org/jepria/compat/Compat.java
@@ -1,10 +1,18 @@
 package org.jepria.compat;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import org.jepria.shared.RecordDefinition;
+
+import com.technology.jep.jepria.shared.JepRiaConstant;
+import com.technology.jep.jepria.shared.field.JepLikeEnum;
+import com.technology.jep.jepria.shared.field.JepTypeEnum;
 import com.technology.jep.jepria.shared.record.JepRecord;
+import com.technology.jep.jepria.shared.record.JepRecordDefinition;
 
 /**
  * Временный класс для поддержания обратной совместимости "новой" и "старой" архитектур Jerpia
@@ -12,6 +20,9 @@ import com.technology.jep.jepria.shared.record.JepRecord;
 @Deprecated
 public class Compat {
   
+  /**
+   * Convert Map(String, Object) to JepRecord
+   */
   public static JepRecord convertRecord(Map<String, Object> map) {
     if (map == null) {
       return null;
@@ -22,11 +33,54 @@ public class Compat {
     return rec;
   }
   
+  /**
+   * Convert List(Map(String, Object)) to List(JepRecord)
+   */
   public static List<Map<String, Object>> convertList(List<JepRecord> list) {
     if (list == null) {
       return null;
     }
     
     return list.stream().map(rec -> (Map<String, Object>)rec).collect(Collectors.toList());
+  }
+  
+  /**
+   * Convert JepRecordDefinition to RecordDefiniton
+   */
+  public static RecordDefinition convertRecordDefinition(JepRecordDefinition jepRecordDefinition) {
+    if (jepRecordDefinition == null) {
+      return null;
+    }
+    
+    return new RecordDefinition() {
+      
+      @Override
+      public List<String> getPrimaryKey() {
+        if (jepRecordDefinition.getPrimaryKey() == null) {
+          return null;
+        }
+        return Arrays.asList(jepRecordDefinition.getPrimaryKey());
+      }
+      
+      @Override
+      public int getMaxResultsetSize() {
+        return JepRiaConstant.DEFAULT_MAX_ROW_COUNT;
+      }
+      
+      @Override
+      public Map<String, JepTypeEnum> getFieldTypes() {
+        return jepRecordDefinition.getTypeMap();
+      }
+      
+      @Override
+      public Map<String, JepLikeEnum> getFieldMatch() {
+        return jepRecordDefinition.getLikeMap();
+      }
+      
+      @Override
+      public Map<String, Comparator<Object>> getFieldComparators() {
+        return null;
+      }
+    }; 
   }
 }
