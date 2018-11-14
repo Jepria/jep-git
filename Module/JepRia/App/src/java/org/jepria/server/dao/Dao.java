ce6d87bbbe61feb1b06818c969db687770569dff
remitot <call9096463086@gmail.com>
2018-11-14 11:02:12 +0300

    refactor(Jepria): сортировка списка переехала в Dao

--- Module/JepRia/App/src/java/org/jepria/server/dao/Dao.java
+++ Module/JepRia/App/src/java/org/jepria/server/dao/Dao.java
@@ -1,5 +1,8 @@
 package org.jepria.server.dao;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -7,6 +10,55 @@ import java.util.Map;
  * Базовый интерфейс работы с записями в БД на прикладном уровне.
  */
 public interface Dao {
+  
+  default void sort(List<?> dataset, LinkedHashMap<String, Comparator<Object>> fieldComparators) {
+    
+    if (dataset == null) {
+      return;
+    }
+
+    final Comparator<Map<String, ?>> recordComparator = new Comparator<Map<String, ?>>() {
+      @Override
+      public int compare(Map<String, ?> r1, Map<String, ?> r2) {
+        
+        if (r1 == null || r2 == null) {
+          if (r1 == null && r2 == null) {
+            return 0;
+          } else {
+            // nulls are the last
+            return r1 == null ? 1 : -1;
+          }
+        }
+        
+        if (fieldComparators != null) {
+          
+          for (String fieldName: fieldComparators.keySet()) {
+            
+            final Object v1 = r1.get(fieldName);
+            final Object v2 = r2.get(fieldName);
+            
+            Comparator<Object> fieldComparator = fieldComparators.get(fieldName);
+            
+            int cmpResult = fieldComparator.compare(v1,  v2);
+            
+            if (cmpResult != 0) {
+              // return immediately on the first differing field
+              return cmpResult;
+            }
+          }
+        }
+        
+        // условно считаем записи равными
+        return 0;
+      }
+    };
+    
+    
+    // assert dataset is of type List<Map<String, ?>>
+    Collections.sort((List<Map<String, ?>>)dataset, recordComparator);
+    
+  }
+  
   /**
    * Поиск.
    * 
--- Module/JepRia/App/src/java/org/jepria/server/service/rest/ResourceSearchControllerBase.java
+++ Module/JepRia/App/src/java/org/jepria/server/service/rest/ResourceSearchControllerBase.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
@@ -14,11 +15,10 @@ import javax.servlet.http.HttpSession;
 
 import org.jepria.compat.CoreCompat;
 import org.jepria.server.dao.Dao;
-import org.jepria.server.load.ColumnSortConfiguration;
-import org.jepria.server.load.ListSorter;
 import org.jepria.server.load.rest.ColumnSortConfigurationDto;
 import org.jepria.server.load.rest.SearchParamsDto;
 import org.jepria.server.security.Credential;
+import org.jepria.shared.RecordDefinition;
 
 import com.technology.jep.jepria.shared.field.JepLikeEnum;
 import com.technology.jep.jepria.shared.field.JepTypeEnum;
@@ -235,7 +235,7 @@ public class ResourceSearchControllerBase implements ResourceSearchController {
     
     SearchModel searchModel = createSearchModel(searchParams);
     
-    List<Map<String, ?>> resultset;
+    List<?> resultset;
     
     try {
       Dao dao = resourceDescription.getDao();
@@ -266,49 +266,46 @@ public class ResourceSearchControllerBase implements ResourceSearchController {
       throw new IllegalStateException("The session attribute must have already been set at this point");
     }
     
-    // collect columnSortConfigurations
-    final List<ColumnSortConfiguration> columnSortConfigurations = convertColumnSortConfigurations(searchParams.getListSortConfiguration());
     
-    // collect field comparators      
-    final Map<String, Comparator<Object>> fieldComparators = new HashMap<>();
-    if (resourceDescription.getRecordDefinition().getFieldNames() != null) {
-      resourceDescription.getRecordDefinition().getFieldNames().forEach(fieldName -> fieldComparators.put(fieldName,
-          resourceDescription.getRecordDefinition().getFieldComparator(fieldName)));
-    }
-    
-    
-    
-    final List<?> resultset = getSessionResultset();
-    
-    if (resultset == null) {
-      throw new IllegalStateException("The session attribute must have already been set at this point");
-    }
+    List<ColumnSortConfigurationDto> listSortConfiguration = searchParams.getListSortConfiguration();
+    if (listSortConfiguration != null) {
+      
+      final LinkedHashMap<String, Comparator<Object>> fieldComparators = new LinkedHashMap<>();
+      
+      for (ColumnSortConfigurationDto columnSortConfiguration: listSortConfiguration) {
+      
+        final String fieldName = columnSortConfiguration.getColumnName();
+        final int sortOrder = "desc".equals(columnSortConfiguration.getSortOrder()) ? -1 : 1; 
+            
+        Comparator<Object> fieldComparatorBase0 = resourceDescription.getRecordDefinition().getFieldComparator(fieldName);
+        final Comparator<Object> fieldComparatorBase = fieldComparatorBase0 != null ? fieldComparatorBase0 : CoreCompat.getDefaultComparator();  
+        
+        // apply sort order
+        Comparator<Object> fieldComparatorOrdered = new Comparator<Object>() {
+          @Override
+          public int compare(Object o1, Object o2) {
+            return fieldComparatorBase.compare(o1, o2) * sortOrder;
+          }
+        };
+        
+        fieldComparators.putIfAbsent(fieldName, fieldComparatorOrdered);
+      }
+      
+      
+      final List<?> resultset = getSessionResultset();
+      
+      if (resultset == null) {
+        throw new IllegalStateException("The session attribute must have already been set at this point");
+      }
 
-    
-    Collections.sort((List<Map<String, ?>>)resultset, new ListSorter(columnSortConfigurations, fieldComparators));
-    // sorting affects the session attribute as well 
-    
-    setSessionResultsetSortValid(true);
-  }
-  
-  /**
-   * Преобразует конфигурацию сортировки столбцов списка из {@link ColumnSortConfigurationDto} в {@link ColumnSortConfiguration}
-   * @param list
-   * @return or null if the input is null
-   */
-  protected static List<ColumnSortConfiguration> convertColumnSortConfigurations(List<ColumnSortConfigurationDto> list) {
-    if (list == null) {
-      return null;
+      
+      Dao dao = resourceDescription.getDao();
+      dao.sort(resultset, fieldComparators);
+      // sorting affects the session attribute as well 
+      
+      setSessionResultsetSortValid(true);
+      
     }
-    
-    List<ColumnSortConfiguration> ret = new ArrayList<>();
-    for (ColumnSortConfigurationDto item: list) {
-      ColumnSortConfiguration columnSortConfiguration = new ColumnSortConfiguration(
-          item.getColumnName(),
-          "desc".equals(item.getSortOrder()) ? -1 : 1);
-      ret.add(columnSortConfiguration);
-    }
-    return ret;
   }
   
   @Override
