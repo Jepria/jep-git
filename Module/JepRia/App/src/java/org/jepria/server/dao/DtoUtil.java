af53ec86c680bbd294f4de0be7e251d7b9082895
remitot <call9096463086@gmail.com>
2018-11-26 18:51:23 +0300

    fix(Jepria): Dto binding

--- Module/JepRia/App/src/java/org/jepria/server/dao/DtoUtil.java
+++ Module/JepRia/App/src/java/org/jepria/server/dao/DtoUtil.java
@@ -16,16 +16,15 @@ public class DtoUtil {
   private DtoUtil() {}
   
   public static Map<String, ?> dtoToMap(Object dto) {
-    final Type type = new HashMap<String, Object>().getClass().getGenericSuperclass();
-    Jsonb jsonb = JsonbBuilder.create();
+    final Type type = new HashMap<String, Object>().getClass();
+    final Jsonb jsonb = JsonbBuilder.create();
     final Map<String, ?> map = jsonb.fromJson(jsonb.toJson(dto), type);
     return map;
   }
   
   public static <T> T mapToDto(Map<String, ?> map, Class<T> dtoClass) {
-    final Type type = dtoClass.getGenericSuperclass();
-    Jsonb jsonb = JsonbBuilder.create();
-    T resource = jsonb.fromJson(jsonb.toJson(map), type);
+    final Jsonb jsonb = JsonbBuilder.create();
+    final T resource = jsonb.fromJson(jsonb.toJson(map), dtoClass);
     return resource;
   }
   
